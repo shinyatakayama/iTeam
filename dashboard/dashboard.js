a5d05c544807c7e757775052a0678b251ceec3f6
shinyatakayama <snytkym7@gmail.com>
2020-10-16 22:52:03 +0900

    20201016

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -50,4 +50,28 @@
       }
     }
   })
+
+  var ctx = document.getElementById("myPieChart");
+  var myPieChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ["A型", "O型", "B型", "AB型"],
+      datasets: [{
+          backgroundColor: [
+              "#BB5179",
+              "#FAFF67",
+              "#58A27C",
+              "#3C00FF"
+          ],
+          data: [38, 31, 21, 10]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '血液型 割合'
+      }
+    }
+  });
+
 }())
